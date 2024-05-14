c24f0e89eb0a524dfd00462f529694e52e5112da
Marc Poljak <marc.poljak@di-unternehmer.com>
2024-05-14 09:00:52 +0200

    Structures

--- src/lectures/exercise/structs/structs.go
+++ src/lectures/exercise/structs/structs.go
@@ -14,11 +14,36 @@
 //--Notes:
 //* The area of a rectangle is length*width
 //* The perimeter of a rectangle is the sum of the lengths of all sides
+//* The perimeter of a rectangle is the sum of the lengths of all sides
 
 package main
 
 import "fmt"
 
+type Rectangle struct {
+	length float64
+	width  float64
+}
+
+func area(r Rectangle) float64 {
+	return r.length * r.width
+}
+
+func perimeter(r Rectangle) float64 {
+	return (r.length + r.width) * 2
+}
+
+func printResults(r Rectangle) {
+	fmt.Println("Area:", area(r))
+	fmt.Println("Perimeter:", perimeter(r))
+}
+
 func main() {
+	r := Rectangle{length: 10, width: 5}
+	printResults(r)
+
+	r.length *= 2
+	r.width *= 2
+	printResults(r)
 
 }
