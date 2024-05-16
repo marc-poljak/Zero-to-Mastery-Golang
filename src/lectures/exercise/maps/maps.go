910cf9b32544a076e02bf1faef3c5b51c900c481
Marc Poljak <marc.poljak@di-unternehmer.com>
2024-05-16 08:58:49 +0200

    Maps

--- src/lectures/exercise/maps/maps.go
+++ src/lectures/exercise/maps/maps.go
@@ -29,6 +29,63 @@ const (
 	Retired     = 3
 )
 
+//* Create a function to print server status displaying:
+func printServerStatus(servers map[string]int) {
+	//  - Number of servers
+	fmt.Println("\nThere are", len(servers), "servers")
+
+	stats := make(map[int]int)
+	for _, status := range servers {
+		switch status {
+		case Online:
+			stats[Online] += 1
+		case Offline:
+			stats[Offline] += 1
+		case Maintenance:
+			stats[Maintenance] += 1
+		case Retired:
+			stats[Retired] += 1
+		default:
+			panic("unhandled server status")
+		}
+	}
+
+	//  - Number of servers for each status (Online, Offline, Maintenance, Retired)
+	fmt.Println(stats[Online], "servers are online")
+	fmt.Println(stats[Offline], "servers are offline")
+	fmt.Println(stats[Maintenance], "servers are undergoing maintenance")
+	fmt.Println(stats[Retired], "servers are retired")
+}
+
 func main() {
 	servers := []string{"darkstar", "aiur", "omicron", "w359", "baseline"}
+
+	//* Create a map using the server names as the key and the server status
+	//  as the value
+	serverStatus := make(map[string]int)
+	//* Set all of the server statuses to `Online` when creating the map
+	for _, server := range servers {
+		serverStatus[server] = Online
+	}
+
+	//* After creating the map, perform the following actions:
+	//  - call display server info function
+	printServerStatus(serverStatus)
+
+	//  - change server status of `darkstar` to `Retired`
+	serverStatus["darkstar"] = Retired
+	//  - change server status `aiur` to `Offline`
+	serverStatus["aiur"] = Offline
+
+	//  - call display server info function
+	printServerStatus(serverStatus)
+
+	//  - change server status all servers to `Maintenance`
+	for server, status := range serverStatus {
+		if status == Online {
+			serverStatus[server] = Maintenance
+		}
+	}
+	//  - call display server info function
+	printServerStatus(serverStatus)
 }
